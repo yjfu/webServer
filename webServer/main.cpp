1cb5017ceb2511f596ee4de86bc4f3418fd8227f
yjfu <15253112679@163.com>
2017-05-21 10:33:57 +0800

    a simple http server
    create project

---
+++ webServer/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  webServer
+//
+//  Created by 傅永健 on 2017/5/21.
+//  Copyright (c) 2017 傅永健. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
